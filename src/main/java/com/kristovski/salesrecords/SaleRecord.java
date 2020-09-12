315da3e26af96879ca529a417e6a73444b194e0d
Krzysiek <kfilak@onet.eu>
2020-09-12 18:33:23 +0200

    added setters and fix bigDecimal error

--- src/main/java/com/kristovski/salesrecords/SaleRecord.java
+++ src/main/java/com/kristovski/salesrecords/SaleRecord.java
@@ -2,6 +2,7 @@ package com.kristovski.salesrecords;
 
 import javax.xml.crypto.Data;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
@@ -11,7 +12,9 @@ public class SaleRecord {
     private LocalDate saleDate;
     private BigDecimal sale;
 
-    private static BigDecimal bigDecimal = new BigDecimal(50.00);
+    private static double val = 50.00;
+    private static BigDecimal bigDecimal = new BigDecimal(val);
+
     private static LocalDate date = LocalDate.of(2020,9,12);
 
     public static final SaleRecord SALE_RECORD = new SaleRecord(1, date , bigDecimal);
@@ -36,5 +39,17 @@ public class SaleRecord {
 
     public BigDecimal getSale() {
         return sale;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setSaleDate(LocalDate saleDate) {
+        this.saleDate = saleDate;
+    }
+
+    public void setSale(BigDecimal sale) {
+        this.sale = sale;
     }
 }
